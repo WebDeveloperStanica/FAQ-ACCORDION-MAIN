086de45a05036d3549332ed98a5671e19091dac0
Aurelian <aurelianstanica30@gmail.com>
2025-05-12 15:19:21 +0100

    I believe I finishes this project, I will now push it to github and publish it to Netlify

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let btn = document.querySelectorAll('.plus-btn');
-let textReveal = document.querySelectorAll('.hideJs');
+let btn = document.querySelectorAll('.plus-btn'); //Selecting the buttons and store it in the btn variable
+let textReveal = document.querySelectorAll('.hideJs'); //Selecting the paragraphs and store it in the textReveal variable
 
-    for (let i = 0; i < btn.length; i++) {
+    for (let i = 0; i < btn.length; i++) {  //Looping through the buttons
 
-        btn[i].addEventListener('click', function() {
+        btn[i].addEventListener('click', function() {  //Adding event listener
 
-            textReveal[i].classList.toggle('reveal');
+            textReveal[i].classList.toggle('reveal');  //
+            //Storing the btn in a img variable
+            let img = btn[i].querySelector('img');
+            //Swaping plus and minus icon signs by using a if/else conditional
+            if(img.src.includes('icon-plus.svg')) {
+                img.src = '/assets/images/icon-minus.svg';
+            } else {
+                img.src = '/assets/images/icon-plus.svg';
+            }
         });
     };
